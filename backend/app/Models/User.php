d52d8d13c0e9bb223755683b77891a2d753f1dc4
tuanba <tuanbui.10103@gmail.com>
2025-06-04 16:53:06 +0700

    Revert "feat: create model"
    This reverts commit 0325a8cbe45603fe00b2cb6ce71878314eced512.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,38 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
-        'firstname',
-        'lastname',
-        'phone_number',
-        'dob',
+        'name',
         'email',
         'password',
-        'role_id'
     ];
 
-    protected $hidden = ['password'];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-    public function role()
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->belongsTo(Role::class);
-    }
-
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
-
-    public function payments()
-    {
-        return $this->hasMany(Payment::class);
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
